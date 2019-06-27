40feb516e2668d366b2eba5ca18cf539099302b6
Ashley Oelbaum <ashleyoelbaum@gmail.com>
2019-06-27 13:30:03 -0400

    woring on implementing delete functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import React, { Component } from 'react';
 import List from './List'
 import './App.css';
+import STORE from './STORE'
+
+
 
 class App extends Component {
-  static defaultProps = {
-    store: {
-      lists: [],
-      allCards: {},
-    }
-  };
+  
+
+  state = {
+    store: STORE
+    
+  }
+  
+  handleDeleteCard = () => {
+    console.log('handleDelete ran')
+    
+    let newCardList = this.state.store.lists.map(list => ({
+      ...list,
+      cardIds: list.cardIds.filter(id => id === this.state.store.allCards[id])
+
+    }))
+
+
+    this.setState({
+      store: {
+        lists: newCardList,
+        allCards: {},
+
+      }
+    }) 
+    console.log(this.state);   
+
+  }
+  
 
   render() {
-    const { store } = this.props
+
+    const { store } = this.state
     return (
       <main className='App'>
         <header className='App-header'>
@@ -20,11 +46,15 @@ class App extends Component {
         <div className='App-list'>
           {store.lists.map(list => (
             <List
-              key={list.id}
+              key={list.id}              
               header={list.header}
               cards={list.cardIds.map(id => store.allCards[id])}
+              deleteClick={this.handleDeleteCard}
+
             />
+            
           ))}
+          
         </div>
       </main>
     );
--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Card.css';
 
+
 export default function Card(props) {
   return (
     <div className='Card'>
-      <button
+      <button onClick = {props.handleDeleteCard} 
         type='button'
       >
         delete
